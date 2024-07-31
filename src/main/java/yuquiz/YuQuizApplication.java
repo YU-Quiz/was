8854bc1cfbebf134ca150830c2107cd9078b412e
DaeYoung Park <pdy000726@naver.com>
2024-07-31 15:40:53 +0900

    chore - first commit

---
+++ src/main/java/yuquiz/YuQuizApplication.java
@@ -0,0 +1,13 @@
+package yuquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yuquiz/YuQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package yuquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
