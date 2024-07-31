9eeccb17622555fac5fb9b154a0f33f28525bc79
DaeYoung Park <pdy000726@naver.com>
2024-07-31 18:13:34 +0900

    feat: Comment Entity, Repository 작성

--- src/main/java/yuquiz/domain/comment/entity/Comment.java
+++ src/main/java/yuquiz/domain/comment/entity/Comment.java
@@ -1,2 +1,36 @@
-package yuquiz.domain.comment.entity;public class Comment {
+package yuquiz.domain.comment.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import yuquiz.domain.post.entity.Post;
+import yuquiz.domain.user.entity.User;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id")
+    private User writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Builder
+    public Comment(String content) {
+        this.content = content;
+    }
 }
--- src/main/java/yuquiz/domain/comment/repository/CommentRepository.java
+++ src/main/java/yuquiz/domain/comment/repository/CommentRepository.java
@@ -1,2 +1,7 @@
-package yuquiz.domain.comment.repository;public class CommentRepository {
+package yuquiz.domain.comment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import yuquiz.domain.comment.entity.Comment;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
