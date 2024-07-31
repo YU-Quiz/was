7621540f3e201d97c7022c8054df746c29283a0b
DaeYoung Park <pdy000726@naver.com>
2024-07-31 18:10:56 +0900

    feat: QuizLike Entity, Repository 작성

--- src/main/java/yuquiz/domain/quizLike/repository/QuizLikeRepository.java
+++ src/main/java/yuquiz/domain/quizLike/repository/QuizLikeRepository.java
@@ -1,2 +1,7 @@
-package yuquiz.domain.quizLike.repository;public class QuizLikeRepository {
+package yuquiz.domain.quizLike.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import yuquiz.domain.quizLike.entity.QuizLike;
+
+public interface QuizLikeRepository extends JpaRepository<QuizLike, Long> {
 }
