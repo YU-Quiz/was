56d5f0595fc50a9727e148c7e2fcd5a74c95c4a6
DaeYoung Park <pdy000726@naver.com>
2024-07-31 18:16:13 +0900

    feat: Comment BaseTimeEntity 상속 추가

--- src/main/java/yuquiz/domain/comment/entity/Comment.java
+++ src/main/java/yuquiz/domain/comment/entity/Comment.java
@@ -6,13 +6,14 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import yuquiz.common.entity.BaseTimeEntity;
 import yuquiz.domain.post.entity.Post;
 import yuquiz.domain.user.entity.User;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Comment {
+public class Comment extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
