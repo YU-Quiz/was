f080ea7ea7452b89534f4e1fd90f563597522a63
DaeYoung Park <pdy000726@naver.com>
2024-08-01 00:26:39 +0900

    feat: api 요청 성공 시, 응답 문구 포맷

--- src/main/java/yuquiz/common/api/SuccessRes.java
+++ src/main/java/yuquiz/common/api/SuccessRes.java
@@ -1,2 +1,21 @@
-package yuquiz.common.api;public class SuccessRes {
+package yuquiz.common.api;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class SuccessRes<T> {
+
+    private T response;
+
+    @Builder
+    private SuccessRes(T response) {
+        this.response = response;
+    }
+
+    public static <T> SuccessRes<T> from(T response) {
+        return SuccessRes.<T>builder()
+                .response(response)
+                .build();
+    }
 }
