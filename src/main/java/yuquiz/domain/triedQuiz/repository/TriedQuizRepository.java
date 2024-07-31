3ac350f8518ded349ee1d20e9d81b1d2709754f1
DaeYoung Park <pdy000726@naver.com>
2024-07-31 18:09:54 +0900

    feat: TriedQuiz Entity, Repository 작성

--- src/main/java/yuquiz/domain/triedQuiz/repository/TriedQuizRepository.java
+++ src/main/java/yuquiz/domain/triedQuiz/repository/TriedQuizRepository.java
@@ -1,2 +1,7 @@
-package yuquiz.domain.triedQuiz.repository;public interface TriedQuizRepository {
+package yuquiz.domain.triedQuiz.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import yuquiz.domain.triedQuiz.entity.TriedQuiz;
+
+public interface TriedQuizRepository extends JpaRepository<TriedQuiz, Long> {
 }
