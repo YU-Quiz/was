d0a5981f66134438bb024ddef0346be570f68f6c
DaeYoung Park <pdy000726@naver.com>
2024-07-31 18:10:41 +0900

    feat: SubSubjects Entity, Repository 작성

--- src/main/java/yuquiz/domain/subSubjects/entity/SubSubjects.java
+++ src/main/java/yuquiz/domain/subSubjects/entity/SubSubjects.java
@@ -1,2 +1,26 @@
-package yuquiz.domain.subSubjects.entity;public class SubSubjects {
+package yuquiz.domain.subSubjects.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import yuquiz.domain.subject.entity.Subject;
+import yuquiz.domain.user.entity.User;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class SubSubjects {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_id")
+    private Subject subject;
 }
--- src/main/java/yuquiz/domain/subSubjects/repository/SubSubjectsRepository.java
+++ src/main/java/yuquiz/domain/subSubjects/repository/SubSubjectsRepository.java
@@ -1,2 +1,7 @@
-package yuquiz.domain.subSubjects.repository;public interface SubSubjectsRepository {
+package yuquiz.domain.subSubjects.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import yuquiz.domain.subSubjects.entity.SubSubjects;
+
+public interface SubSubjectsRepository extends JpaRepository<SubSubjects, Long> {
 }
