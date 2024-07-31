0f188eebec99bcaa677a0edf916b9f7eab4aeac6
DaeYoung Park <pdy000726@naver.com>
2024-07-31 18:11:57 +0900

    feat: Post Entity, Repository 작성

--- src/main/java/yuquiz/domain/post/repository/PostRepository.java
+++ src/main/java/yuquiz/domain/post/repository/PostRepository.java
@@ -1,2 +1,7 @@
-package yuquiz.domain.post.repository;public interface PostRepository {
+package yuquiz.domain.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import yuquiz.domain.post.entity.Post;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
