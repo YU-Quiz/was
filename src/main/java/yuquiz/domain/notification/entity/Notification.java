eb75d6931c6890b99bbdaf6285bf0f621caa891c
DaeYoung Park <pdy000726@naver.com>
2024-08-04 15:59:34 +0900

    feat: Notificaation 필드 추가

--- src/main/java/yuquiz/domain/notification/entity/Notification.java
+++ src/main/java/yuquiz/domain/notification/entity/Notification.java
@@ -1,6 +1,7 @@
 package yuquiz.domain.notification.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,13 @@ public class Notification extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    @Column(name = "redirect_url")
+    private String redirectUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
