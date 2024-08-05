612869acc1e1daa5789870a24eee7ee6992aae43
DaeYoung Park <pdy000726@naver.com>
2024-08-05 12:47:12 +0900

    feat: 사용자 회원가입 구현

--- src/main/java/yuquiz/domain/user/dto/SignUpReq.java
+++ src/main/java/yuquiz/domain/user/dto/SignUpReq.java
@@ -1,5 +1,8 @@
 package yuquiz.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,14 +10,29 @@ import yuquiz.domain.user.entity.User;
 
 @Getter @Setter
 @Builder
-public class SignUpDto {
+public class SignUpReq {
 
+    @NotBlank(message = "아이디는 필수 입력입니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{4,20}$", message = "아이디는 특수문자를 제외한 4~20자리여야 합니다.")
     private String username;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z]).{8,16}", message = "비밀번호는 8~16자 영문과 숫자를 사용하세요.")
     private String password;
+
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String nickname;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
     private String email;
+
+    @NotBlank(message = "학과는 필수 선택 값입니다.")
     private String majorName;
+
     private boolean agreeEmail;
+
 
     public User toEntity() {
         return User.builder()
