6342a1f43b227e34b8a32e2ae4b2d84191645d48
DaeYoung Park <pdy000726@naver.com>
2024-08-09 17:56:31 +0900

    feat: 로그아웃한 사용자의 accessToken blackList에 저장

--- src/main/java/yuquiz/security/token/blacklist/BlackListTokenService.java
+++ src/main/java/yuquiz/security/token/blacklist/BlackListTokenService.java
@@ -1,2 +1,32 @@
-package yuquiz.security.token.blacklist;public class BlackListTokenService {
+package yuquiz.security.token.blacklist;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import yuquiz.common.utils.redis.RedisUtil;
+
+import static yuquiz.common.utils.redis.RedisProperties.BLACKLIST_KEY_PREFIX;
+
+@Service
+@RequiredArgsConstructor
+public class BlackListTokenService {
+
+    private final RedisUtil redisUtil;
+
+    @Value("${token.blacklist.in-redis}")
+    private long BLACKLIST_EXPIRATION_TIME;
+
+    /* redis에 저장 */
+    public void saveBlackList(String accessToken) {
+
+        String key = BLACKLIST_KEY_PREFIX + accessToken;
+        redisUtil.set(key, accessToken);
+        redisUtil.expire(key, BLACKLIST_EXPIRATION_TIME);
+    }
+
+    /* 블랙리스트 확인. */
+    public boolean existsBlackListCheck(String accessToken) {
+        return redisUtil.existed(BLACKLIST_KEY_PREFIX + accessToken);
+    }
+
 }
