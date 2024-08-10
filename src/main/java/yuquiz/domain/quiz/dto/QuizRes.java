484f1dd8d81180c7f7dd1c22c3cfb8753bd1a179
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-08-10 19:16:10 +0900

    fix: User 순환 참조 해결

--- src/main/java/yuquiz/domain/quiz/dto/QuizRes.java
+++ src/main/java/yuquiz/domain/quiz/dto/QuizRes.java
@@ -1,21 +1,20 @@
 package yuquiz.domain.quiz.dto;
 
 import yuquiz.domain.quiz.entity.Quiz;
-import yuquiz.domain.user.entity.User;
 
 import java.time.LocalDateTime;
 
 public record QuizRes(
         Long id,
         String title,
-        User writer,
+        String username,
         LocalDateTime createdAt
 ) {
     public static QuizRes from(Quiz quiz){
         return new QuizRes(
                 quiz.getId(),
                 quiz.getTitle(),
-                quiz.getWriter(),
+                quiz.getWriter().getUsername(),
                 quiz.getCreatedAt()
         );
     }
