dc1598f96167ef38c4c018d3296bcd5e8a566073
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-08-11 20:50:53 +0900

    feat: dto Swagger 작성

--- src/main/java/yuquiz/domain/quiz/dto/AnswerReq.java
+++ src/main/java/yuquiz/domain/quiz/dto/AnswerReq.java
@@ -1,8 +1,10 @@
 package yuquiz.domain.quiz.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record AnswerReq(
+        @Schema(description = "정답", example = "1")
         @NotBlank(message = "정답은 필수 입력입니다.")
         String answer
 ) {
