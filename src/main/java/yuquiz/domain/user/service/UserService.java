ecf8f8116f52379da3fe78053dff38a33796bcc6
DaeYoung Park <pdy000726@naver.com>
2024-08-11 18:05:23 +0900

    feat: 회원 정보 읽기, 수정, 삭제 구현

--- src/main/java/yuquiz/domain/user/service/UserService.java
+++ src/main/java/yuquiz/domain/user/service/UserService.java
@@ -1,10 +1,16 @@
 package yuquiz.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import yuquiz.common.exception.CustomException;
 import yuquiz.domain.user.dto.SignUpReq;
+import yuquiz.domain.user.dto.UserDetailsRes;
+import yuquiz.domain.user.dto.UserUpdateReq;
+import yuquiz.domain.user.entity.User;
+import yuquiz.domain.user.exception.UserExceptionCode;
 import yuquiz.domain.user.repository.UserRepository;
 
 @Service
@@ -21,4 +27,42 @@ public class UserService {
         String encodePassword = passwordEncoder.encode(signUpReq.password());
         userRepository.save(signUpReq.toEntity(encodePassword));
     }
+
+    /* 사용자 정보 불러오기 */
+    @Transactional(readOnly = true)
+    public UserDetailsRes getUserInfo(Long userId) {
+
+        return UserDetailsRes.fromEntity(findUserByUserId(userId));
+    }
+
+    /* 사용자 정보 업데이트 */
+    @Transactional
+    public void updateUserInfo(UserUpdateReq updateReq, Long userId) {
+
+        User foundUser = findUserByUserId(userId);
+
+        String encodePassword = passwordEncoder.encode(updateReq.password());
+
+        foundUser.updateUser(encodePassword, updateReq.nickname(),
+                updateReq.email(), updateReq.agreeEmail(), updateReq.majorName());
+    }
+
+    /* 사용자 정보 삭제 */
+    @Transactional
+    public void deleteUserInfo(Long userId) {
+
+        try {
+            userRepository.deleteById(userId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new CustomException(UserExceptionCode.INVALID_USERID);
+        }
+    }
+
+    /* 사용자 불러오기 */
+    private User findUserByUserId(Long userId) {
+
+        return userRepository.findById(userId).orElseThrow(() ->
+                new CustomException(UserExceptionCode.INVALID_USERID));
+    }
+
 }
