962cb012fff0e76e10262e85152919d551dc7329
DaeYoung Park <pdy000726@naver.com>
2024-08-11 18:04:46 +0900

    feat: User dto 추가

---
+++ src/main/java/yuquiz/domain/user/dto/UserUpdateReq.java
@@ -0,0 +1,25 @@
+package yuquiz.domain.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record UserUpdateReq(
+        @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z]).{8,16}", message = "비밀번호는 8~16자 영문과 숫자를 사용하세요.")
+        String password,
+
+        @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+        String nickname,
+
+        @NotBlank(message = "이메일은 필수 입력 값입니다.")
+        @Email(message = "유효한 이메일 형식이 아닙니다.")
+        String email,
+
+        @NotBlank(message = "학과는 필수 선택 값입니다.")
+        String majorName,
+
+        boolean agreeEmail
+) {
+}
