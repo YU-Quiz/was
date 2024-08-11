0e2371f998dcfca1fd690e4f163618a15918df9f
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-08-12 00:57:09 +0900

    refact: 코드 일관성 수정

--- src/main/java/yuquiz/domain/report/controller/AdminReportController.java
+++ src/main/java/yuquiz/domain/report/controller/AdminReportController.java
@@ -1,11 +1,15 @@
 package yuquiz.domain.report.controller;
 
-import jakarta.validation.constraints.Min;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
 import yuquiz.domain.report.dto.ReportSummaryRes;
 import yuquiz.domain.report.service.AdminReportService;
 
@@ -17,10 +21,10 @@ public class AdminReportController {
     private final AdminReportService adminReportService;
 
     @GetMapping
-    public ResponseEntity<?> getReportPage(@RequestParam @Min(0) Integer pageNumber) {
+    public ResponseEntity<?> getReportPage(@RequestParam @Min(0) Integer page) {
 
-        Page<ReportSummaryRes> page = adminReportService.getReportPage(pageNumber);
+        Page<ReportSummaryRes> reports = adminReportService.getReportPage(page);
 
-        return ResponseEntity.status(HttpStatus.OK).body(page);
+        return ResponseEntity.status(HttpStatus.OK).body(reports);
     }
 }
