4b758d7474219fff519f1fb154b4101740d47aa0
DaeYoung Park <pdy000726@naver.com>
2024-08-26 20:04:52 +0900

    feat: 인증번호 관련 문구 추가

--- src/main/java/yuquiz/common/utils/redis/RedisProperties.java
+++ src/main/java/yuquiz/common/utils/redis/RedisProperties.java
@@ -4,4 +4,6 @@ public interface RedisProperties {
 
     String REFRESH_TOKEN_KEY_PREFIX = "refreshToken::";
     String BLACKLIST_KEY_PREFIX = "blackList::";
+    String CODE_KEY_PREFIX = "code::";
+    long CODE_EXPIRATION_TIME = 3*60;
 }
