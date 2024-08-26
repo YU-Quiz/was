b38997cf4b63b65f0f6549e8bb644b485a11b246
DaeYoung Park <pdy000726@naver.com>
2024-08-26 19:42:34 +0900

    feat: 메일 전송 로직 작성

---
+++ src/main/java/yuquiz/common/mail/service/MailService.java
@@ -0,0 +1,82 @@
+package yuquiz.common.mail.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
+import yuquiz.common.exception.CustomException;
+import yuquiz.common.exception.exceptionCode.GlobalExceptionCode;
+import yuquiz.common.mail.MailType;
+
+import java.io.UnsupportedEncodingException;
+
+import static yuquiz.common.mail.MailProperties.CODE_SUBJECT;
+import static yuquiz.common.mail.MailProperties.CODE_TEXT;
+import static yuquiz.common.mail.MailProperties.MAIL_SENDER;
+import static yuquiz.common.mail.MailProperties.PASS_SUBJECT;
+import static yuquiz.common.mail.MailProperties.PASS_TEXT;
+
+@RequiredArgsConstructor
+@EnableAsync
+@Service
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String emailUsername;
+
+    @Value("${web.post-url}")
+    private String postURL;
+
+    /* 메일 보내기 */
+    @Async
+    public void sendMail(String email, String content, MailType type) {
+
+        MimeMessage message = null;
+
+        try {
+            switch (type) {
+                case CODE -> message = getCodeMessage(email, content);
+                case PASS -> message = getPassMessage(email, content);
+            }
+            javaMailSender.send(message);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            throw new CustomException(GlobalExceptionCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /* Code Message 만들기. */
+    private MimeMessage getCodeMessage(String email, String code) throws MessagingException, UnsupportedEncodingException  {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setBcc(email);
+        helper.setSubject(CODE_SUBJECT);
+        helper.setText(CODE_TEXT + code, true);
+        helper.setFrom(new InternetAddress(emailUsername, MAIL_SENDER, "UTF-8"));
+
+        return message;
+    }
+
+    /* Pass Message 만들기. */
+    private MimeMessage getPassMessage(String email, String pass) throws MessagingException, UnsupportedEncodingException  {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setBcc(email);
+        helper.setSubject(PASS_SUBJECT);
+        helper.setText(PASS_TEXT + pass, true);
+        helper.setFrom(new InternetAddress(emailUsername, MAIL_SENDER, "UTF-8"));
+
+        return message;
+    }
+}
