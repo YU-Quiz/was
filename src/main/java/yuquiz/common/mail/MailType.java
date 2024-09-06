3eaca8afa794ad5e96c68963c72dfb98e81ca11c
DaeYoung Park <pdy000726@naver.com>
2024-09-06 18:24:01 +0900

    refact: 사용하지 않는 메서드 삭제

--- src/main/java/yuquiz/common/mail/MailType.java
+++ src/main/java/yuquiz/common/mail/MailType.java
@@ -1,6 +1,5 @@
 package yuquiz.common.mail;
 
 public enum MailType {
-    CODE,   // 인증 코드
-    PASS   // 비밀번호 찾기
+    CODE   // 인증 코드
 }
---
+++ src/main/java/yuquiz/domain/auth/dto/FindUsernameReq.java
@@ -0,0 +1,2 @@
+package yuquiz.domain.auth.dto;public record FindUsernameReq() {
+}
