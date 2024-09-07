b087115c70106b22908e6335642c19250f540181
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-09-07 14:25:12 +0900

    refact: 불필요 import 삭제

--- src/main/java/yuquiz/domain/report/repository/ReportRepository.java
+++ src/main/java/yuquiz/domain/report/repository/ReportRepository.java
@@ -1,7 +1,5 @@
 package yuquiz.domain.report.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
