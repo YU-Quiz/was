f3d180e71653d48a0adec0fe21fb7db3a8aaa121
DaeYoung Park <pdy000726@naver.com>
2024-09-21 13:56:44 +0900

    feat: 남은 jwt TTL만큼 blacklist 저장

--- src/main/java/yuquiz/security/token/blacklist/BlackListTokenService.java
+++ src/main/java/yuquiz/security/token/blacklist/BlackListTokenService.java
@@ -1,7 +1,6 @@
 package yuquiz.security.token.blacklist;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import yuquiz.common.utils.redis.RedisUtil;
 
@@ -15,9 +14,6 @@ import static yuquiz.common.utils.redis.RedisProperties.BLACKLIST_KEY_PREFIX;
 public class BlackListTokenService {
 
     private final RedisUtil redisUtil;
-
-    @Value("${token.blacklist.in-redis}")
-    private long BLACKLIST_EXPIRATION_TIME;
 
     /* redis에 저장 */
     public void saveBlackList(String accessToken, LocalDateTime expireAt) {
