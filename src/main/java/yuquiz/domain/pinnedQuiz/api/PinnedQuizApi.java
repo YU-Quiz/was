cf12ad22a636ca86b2178da9cddea9fae126474b
이승현 <50231309+Uralauah@users.noreply.github.com>
2024-09-22 13:25:23 +0900

    docs: 퀴즈 관련 Swagger 최신화 (#52)

--- src/main/java/yuquiz/domain/pinnedQuiz/api/PinnedQuizApi.java
+++ src/main/java/yuquiz/domain/pinnedQuiz/api/PinnedQuizApi.java
@@ -19,57 +19,49 @@ public interface PinnedQuizApi {
     @Operation(summary = "퀴즈 즐겨찾기 목록", description = "사용자가 즐겨찾기한 퀴즈 목록 api")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "즐겨찾기 목록 불러오기 성공",
-            content = @Content(mediaType = "application/json", examples = {
-                    @ExampleObject(value = """
-                            {
-                                "totalPages": 1,
-                                "totalElements": 2,
-                                "first": true,
-                                "last": true,
-                                "size": 20,
-                                "content": [
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
                                     {
-                                        "quizId": 7,
-                                        "quizTitle": "testing",
-                                        "nickname": "테스터111",
-                                        "createdAt": "2024-08-11T19:43:53",
-                                        "likeCount": 5,
-                                        "viewCount": 16,
-                                        "isSolved": true
-                                    },
-                                    {
-                                        "quizId": 8,
-                                        "quizTitle": "hello",
-                                        "nickname": "테스터",
-                                        "createdAt": "2024-08-12T13:32:55",
-                                        "likeCount": 2,
-                                        "viewCount": 20,
-                                        "isSolved": false
-                                    }
-                                ],
-                                "number": 0,
-                                "sort": {
-                                    "empty": true,
-                                    "unsorted": true,
-                                    "sorted": false
-                                },
-                                "pageable": {
-                                    "pageNumber": 0,
-                                    "pageSize": 20,
-                                    "sort": {
-                                        "empty": true,
-                                        "unsorted": true,
-                                        "sorted": false
-                                    },
-                                    "offset": 0,
-                                    "unpaged": false,
-                                    "paged": true
-                                },
-                                "numberOfElements": 2,
-                                "empty": false
-                            }
-                            """)
-            }))
+                                         "totalPages": 1,
+                                         "totalElements": 1,
+                                         "first": true,
+                                         "last": true,
+                                         "size": 20,
+                                         "content": [
+                                             {
+                                                 "quizId": 7,
+                                                 "quizTitle": "퀴즈 테스트",
+                                                 "nickname": "테스터",
+                                                 "createdAt": "2024-08-11T19:43:53",
+                                                 "likeCount": 5,
+                                                 "viewCount": 25,
+                                                 "isSolved": true,
+                                                 "quizType": "MULTIPLE_CHOICE"
+                                             }
+                                         ],
+                                         "number": 0,
+                                         "sort": {
+                                             "empty": false,
+                                             "sorted": true,
+                                             "unsorted": false
+                                         },
+                                         "pageable": {
+                                             "pageNumber": 0,
+                                             "pageSize": 20,
+                                             "sort": {
+                                                 "empty": false,
+                                                 "sorted": true,
+                                                 "unsorted": false
+                                             },
+                                             "offset": 0,
+                                             "unpaged": false,
+                                             "paged": true
+                                         },
+                                         "numberOfElements": 1,
+                                         "empty": false
+                                     }
+                                    """)
+                    }))
     })
     ResponseEntity<?> getPinnedQuizzes(
             @AuthenticationPrincipal SecurityUserDetails userDetails,
