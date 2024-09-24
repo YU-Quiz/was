534d090992c9d7404f00385682260ddc3fc692a2
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-09-24 15:40:27 +0900

    feat: Sse를 활용한 실시간 알림 구현

--- src/main/java/yuquiz/domain/notification/controller/NotificationController.java
+++ src/main/java/yuquiz/domain/notification/controller/NotificationController.java
@@ -3,9 +3,11 @@ package yuquiz.domain.notification.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import yuquiz.domain.notification.dto.DisplayType;
 import yuquiz.domain.notification.dto.NotificationRes;
 import yuquiz.domain.notification.dto.NotificationSortType;
@@ -27,5 +29,11 @@ public class NotificationController {
         Page<NotificationRes> notifications = notificationService.getAllNotification(userDetails.getId(), page, sort, displayType);
 
         return ResponseEntity.status(HttpStatus.OK).body(notifications);
+    }
+
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@AuthenticationPrincipal SecurityUserDetails userDetails,
+                                @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
+        return notificationService.subscribe(userDetails.getId(), lastEventId);
     }
 }
--- src/main/java/yuquiz/domain/report/service/ReportService.java
+++ src/main/java/yuquiz/domain/report/service/ReportService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import yuquiz.common.exception.CustomException;
-import yuquiz.domain.notification.dto.NotificationReq;
 import yuquiz.domain.notification.dto.NotificationType;
 import yuquiz.domain.notification.service.NotificationService;
 import yuquiz.domain.quiz.entity.Quiz;
@@ -13,6 +12,7 @@ import yuquiz.domain.quiz.repository.QuizRepository;
 import yuquiz.domain.report.dto.ReportReq;
 import yuquiz.domain.report.entity.Report;
 import yuquiz.domain.report.repository.ReportRepository;
+import yuquiz.domain.user.entity.User;
 
 @Service
 @RequiredArgsConstructor
@@ -28,7 +28,7 @@ public class ReportService {
 
         Report report = reportReq.toEntity(quiz);
 
-        reportNotification(quiz.getTitle(), report.getReason(), quiz.getWriter().getId());
+        reportNotification(quiz);
 
         if (reportRepository.countByQuiz(quiz) + 1 >= 10) {
             quiz.changeVisibility();
@@ -37,14 +37,11 @@ public class ReportService {
         reportRepository.save(report);
     }
 
-    public void reportNotification(String quizTitle, String reason, Long userId) {
-        NotificationReq notificationReq = NotificationReq.builder()
-                .title(quizTitle + " " + NotificationType.QUIZ_REPORT_NOTIFICATION.getTitle())
-                .message(quizTitle + " " + NotificationType.QUIZ_REPORT_NOTIFICATION.getMessage() +
-                        "\n사유 : " + reason)
-                .redirectUrl("")
-                .build();
+    public void reportNotification(Quiz quiz) {
+        User user = quiz.getWriter();
+        String content = "\"" + quiz.getTitle() + "\" 퀴즈에 대한 신고가 있습니다.";
+        String url = "/api/v1/quizzes/"+quiz.getId();
 
-        notificationService.createNotification(userId, notificationReq);
+        notificationService.send(user, NotificationType.REPORT, content, url);
     }
 }
