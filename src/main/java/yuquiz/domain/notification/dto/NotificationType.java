a829820cf60d2d61dd0e3d87e14b01c8f5cce502
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-09-24 15:22:01 +0900

    feat: 알림 타입 설정

--- src/main/java/yuquiz/domain/notification/dto/NotificationType.java
+++ src/main/java/yuquiz/domain/notification/dto/NotificationType.java
@@ -1,13 +1,5 @@
 package yuquiz.domain.notification.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum NotificationType {
-    QUIZ_REPORT_NOTIFICATION("퀴즈에 대한 신고", "퀴즈에 대한 신고가 있습니다.");
-
-    private final String title;
-    private final String message;
+    COMMENT, REPORT, LIKE, STUDY_JOIN_REQUEST, STUDY_JOIN_ACCEPTED, STUDY_JOIN_REJECTED, STUDY_KICKED
 }
