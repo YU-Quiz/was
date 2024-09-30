85ee9f7d56e43e6c0bf40efa7f110266aad1bde3
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-09-30 20:40:41 +0900

    refact: 사용하지 하지 않는 게시글 업데이트 메서드 삭제

--- src/main/java/yuquiz/domain/post/repository/PostRepository.java
+++ src/main/java/yuquiz/domain/post/repository/PostRepository.java
@@ -3,10 +3,8 @@ package yuquiz.domain.post.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import yuquiz.domain.category.entity.Category;
 import yuquiz.domain.post.entity.Post;
 
 import java.util.Optional;
@@ -15,13 +13,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("select p.writer.id from Post p where p.id = :id")
     Optional<Long> findWriterIdById(@Param("id") Long id);
-
-    @Modifying
-    @Query("update Post p set p.title = :title, p.content = :content, p.category = :category where p.id = :id")
-    void updateById(@Param("id") Long id,
-                    @Param("title") String title,
-                    @Param("content") String content,
-                    @Param("category") Category category);
 
     @Query("SELECT p FROM Post p WHERE"
             + "(:keyword IS NULL OR p.title LIKE %:keyword% OR p.content LIKE %:keyword%)"
