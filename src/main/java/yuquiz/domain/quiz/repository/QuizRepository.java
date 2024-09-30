6a36300778ea814ac8d49c5b54baaef7f1508268
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-09-30 11:00:44 +0900

    refact: 비활성화된 퀴즈 퀴즈 목록에서 제외

--- src/main/java/yuquiz/domain/quiz/repository/QuizRepository.java
+++ src/main/java/yuquiz/domain/quiz/repository/QuizRepository.java
@@ -18,9 +18,9 @@ public interface QuizRepository extends JpaRepository<Quiz, Long> {
 
     @Query("SELECT q FROM Quiz q WHERE "
             + "(:keyword IS NULL OR q.title LIKE %:keyword% OR q.question LIKE %:keyword%) "
-            + "AND (:subjectId IS NULL OR q.subject.id = :subjectId)")
+            + "AND (:subjectId IS NULL OR q.subject.id = :subjectId) "
+            + "AND q.visibility = true")
     Page<Quiz> findQuizzesByKeywordAndSubject(@Param("keyword") String keyword, @Param("subjectId") Long subjectId, Pageable pageable);
-
     Page<Quiz> findAllByWriter(User writer, Pageable pageable);
 
     @Query("select q.writer.id from Quiz q where q.id = :id")
