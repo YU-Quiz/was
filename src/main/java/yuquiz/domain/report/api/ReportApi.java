7473db1302bb33dd18c12a1cbd684c1e50915db1
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2024-10-01 14:17:58 +0900

    docs: Swagger 수정

--- src/main/java/yuquiz/domain/report/api/ReportApi.java
+++ src/main/java/yuquiz/domain/report/api/ReportApi.java
@@ -7,9 +7,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import yuquiz.domain.report.dto.ReportReq;
+import yuquiz.security.auth.SecurityUserDetails;
 
 public interface ReportApi {
     @Operation(summary = "퀴즈 신고", description = "퀴즈의 오류 혹은 문제를 신고하는 API")
@@ -31,16 +33,34 @@ public interface ReportApi {
                                     }
                                     """)
                     })),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                        "status": 404,
+                                        "message": "존재하지 않는 사용자입니다."
+                                    }
+                                    """)
+                    })),
             @ApiResponse(responseCode = "400", description = "유효성 검사 실패",
                     content = @Content(mediaType = "application/json", examples = {
                             @ExampleObject(value = """
                                     {
-                                        "reason": "신고 사유는 필수 입력입니다.",
                                         "type": "신고 유형은 필수 입력입니다."
+                                    }
+                                    """)
+                    })),
+            @ApiResponse(responseCode = "409", description = "중복 신고",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                        "status": 409,
+                                        "message": "이미 신고한 퀴즈입니다."
                                     }
                                     """)
                     }))
     })
     ResponseEntity<?> reportQuiz(@PathVariable(value = "quizId") Long quizId,
-                                 @Valid @RequestBody ReportReq reportReq);
+                                 @Valid @RequestBody ReportReq reportReq,
+                                 @AuthenticationPrincipal SecurityUserDetails userDetails);
 }
