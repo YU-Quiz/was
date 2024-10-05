0bc1ebe01bc7344a4b5ace0b70814e1971313921
정원영 <85011588+gardenzeeero@users.noreply.github.com>
2024-10-05 15:20:34 +0900

    feat: 문제집 관련 Entity 설정 (#62)
    * feat: 문제집 관련 Entity 설정
    
    * feat: 퀴즈와 문제집 간의 양방향 매핑 추가
    
    ---------
    
    Co-authored-by: Garden_zero <85011588+Garden-Zero@users.noreply.github.com>

---
+++ src/main/java/yuquiz/domain/quizSeries/entity/QuizSeries.java
@@ -0,0 +1,33 @@
+package yuquiz.domain.quizSeries.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import yuquiz.domain.quiz.entity.Quiz;
+import yuquiz.domain.series.entity.Series;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QuizSeries {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "series_id")
+    private Series series;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+
+    @Builder
+    public QuizSeries(Series series, Quiz quiz) {
+        this.series = series;
+        this.quiz = quiz;
+    }
+}
