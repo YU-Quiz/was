e88f3c611238ce66f2464577db7dcd6427b34bdf
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-10-05 20:12:37 +0900

    feat: dto 유효성 검사 메세지 추가

--- src/main/java/yuquiz/domain/series/dto/SeriesReq.java
+++ src/main/java/yuquiz/domain/series/dto/SeriesReq.java
@@ -1,13 +1,15 @@
 package yuquiz.domain.series.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import yuquiz.domain.series.entity.Series;
 import yuquiz.domain.study.entity.Study;
 import yuquiz.domain.user.entity.User;
 
 public record SeriesReq(
-        @NotBlank
+        @NotBlank(message = "문제집 이름은 필수 입력입니다.")
         String name,
+        @Min(value = 1, message = "스터디ID는 1 이상이어야 합니다.")
         Long studyId
 ) {
 
