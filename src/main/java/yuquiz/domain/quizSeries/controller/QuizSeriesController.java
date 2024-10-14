cbb77218b926c7d21811e29cf003060d761d0850
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2024-10-15 01:21:55 +0900

    docs: 문제집에 퀴즈 추가 및 삭제 기능 Swagger 작성

--- src/main/java/yuquiz/domain/quizSeries/controller/QuizSeriesController.java
+++ src/main/java/yuquiz/domain/quizSeries/controller/QuizSeriesController.java
@@ -6,16 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import yuquiz.common.api.SuccessRes;
+import yuquiz.domain.quizSeries.api.QuizSeriesApi;
 import yuquiz.domain.quizSeries.service.QuizSeriesService;
 import yuquiz.security.auth.SecurityUserDetails;
 
 @RestController
 @RequestMapping("/api/v1/series")
 @RequiredArgsConstructor
-public class QuizSeriesController {
+public class QuizSeriesController implements QuizSeriesApi {
 
     private final QuizSeriesService quizSeriesService;
 
+    @Override
     @PostMapping("/{seriesId}/{quizId}")
     public ResponseEntity<?> addQuizToSeries(@PathVariable(value = "seriesId") Long seriesId,
                                              @PathVariable(value = "quizId") Long quizId,
@@ -26,7 +28,7 @@ public class QuizSeriesController {
         return ResponseEntity.status(HttpStatus.CREATED).body(SuccessRes.from("문제집에 문제 추가 성공"));
     }
 
-
+    @Override
     @DeleteMapping("/{seriesId}/{quizId}")
     public ResponseEntity<?> deleteQuizFromSeries(@PathVariable(value = "seriesId") Long seriesId,
                                                   @PathVariable(value = "quizId") Long quizId,
